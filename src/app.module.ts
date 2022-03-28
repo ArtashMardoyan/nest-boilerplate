2ed450f91b3dcd5554bb56d8674ad9273c9a1640
artash_mardoyan <artashmardoyan@gmail.com>
2022-03-28 17:19:04 +0400

    Updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
+import { UsersModule } from './modules/users/users.module';
 import { SitesModule } from './modules/sites/sites.module';
 import { getMongoConfig } from './config/mongo.config';
 import { AppController } from './app.controller';
 import mainConfig from './config/main.config';
 import { AppService } from './app.service';
-import { UsersModule } from './modules/users/users.module';
 
 @Module({
     imports: [
